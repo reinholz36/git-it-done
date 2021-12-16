942de6709fa9a962c2730410676030362ad7c0ca
reinholz36 <dreinholz28@gmail.com>
2021-12-16 16:26:35 -0500

    updated ui page errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,21 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  //this if filters to make sure a repoName has been entered (if repoName is not blank {})
+  if (repoName) {
+  // display repo name on the page
+  repoNameEl.textContent = repoName
+  getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -20,8 +35,9 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
+      // if not successful, redirect to homepage
       console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -81,4 +97,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName("microsoft/activities");
